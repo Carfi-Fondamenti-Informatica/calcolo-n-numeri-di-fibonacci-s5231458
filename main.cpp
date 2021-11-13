60ff9f28dfa841bd0163faa0a0b1f588a3bace90
Marco Badin <91483735+s5231458@users.noreply.github.com>
2021-11-13 18:14:41 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+
+    int n=0;
+    cin>>n;
+
+    if (n>=2){
+        int a=0, b=1, c=a+b;
+        cout<<b<<endl;
+
+        for(int i=2;i<=n;i++){
+            c=a+b;
+            a=b;
+            b=c;
+            cout << c << endl;
+
+        }
+
+        
+    }else {
+        cout<<"errore"<<endl;
+    }
+
+    
+    return 0;
 }
